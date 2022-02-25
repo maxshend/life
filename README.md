b238686bff20e63e7dec2aee1d07d574d88fa4d5
maxshend <shendrik.mv@gmail.com>
2022-02-25 18:26:20 +0300

    Add a CI github workflow for running specs. Create dependabot.yml.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Life
 
+![CI](https://github.com/maxshend/life/actions/workflows/ci.yml/badge.svg)
+
 Conway's Game Of Life in Ruby
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
-require 'life'
 require 'simplecov'
-require 'simplecov-cobertura'
 
 SimpleCov.start do
   add_filter 'spec/'
-  add_filter './github/'
+  add_filter '.github/'
   add_filter 'lib/life/version'
 end
 
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter if ENV['CI'] == 'true'
+if ENV['CI'] == 'true'
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+end
+
+require 'life'
 
 Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
